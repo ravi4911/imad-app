5c00b30513cccfe0b05f057c227047e2c9f4668f
Ravi Vaniya <ravivaniya4911@gmail.com>
2017-08-09 22:28:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ var counter = 0;
 };*/
 
 button.onclick = function(){
-  counter = counter + 1;
-  var span = document.getElementById('count');
+    counter = counter + 1;
+    var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
