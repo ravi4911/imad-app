0105c4ac421c6de320a0588f5a4eb2074ac4a7b0
Ravi Vaniya <ravivaniya4911@gmail.com>
2017-08-09 16:00:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHtml = 'New javascript';
+
+//moving image
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginLeft='100px';
+};
