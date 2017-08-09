8605a6617d56707132d32ab0b382eff66eb7ab30
Ravi Vaniya <ravivaniya4911@gmail.com>
2017-08-09 22:14:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 
@@ -15,4 +15,30 @@ function moveRight(){
 }
 img.onclick = function() {
     var interval = setInterval(moveRight,100);
+};*/
+
+//code  for counter
+
+var counter = document.getElementById('counter');
+
+button.onclick = function(){
+  
+  //create a request object.
+  var request = new XMLHttpRequest();
+  
+  //capture the response and store it in variable.
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        //take some action
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = count.toString();
+        }
+    }  
+  };
+  
+  //make request
+  request.open('GET','http://ravivaniya4911.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
