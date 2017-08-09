86d1037eadf9b834d1ba0cc3299c7b400ab60bc2
Ravi Vaniya <ravivaniya4911@gmail.com>
2017-08-09 16:08:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,13 @@ element.innerHtml = 'New javascript';
 
 //moving image
 var img = document.getElementById('madi');
+
+var marginLeft = 0;
+
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
-    img.style.marginLeft='100px';
+    var interval = setInterval(moveRight,100);
 };
