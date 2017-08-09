6c89c557532088c544cc7152bd64dffdb7b6aad4
Ravi Vaniya <ravivaniya4911@gmail.com>
2017-08-09 11:29:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,36 +21,46 @@ var articleOne = {
                 </p>`
 };
 
-var htmlTemplate = `
-    <!DOCTYPE html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width=device-width initial-scale=1"/>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-            <div>
-                <a href="/">Home</a>
+
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+        <!DOCTYPE html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr>
+                <div>
+                    <h3>
+                        ${heading}
+                    </h3>
+                </div>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
             </div>
-            <hr>
-            <div>
-                <h3>
-                    ${heading}
-                </h3>
-            </div>
-            <div>
-                ${date}
-            </div>
-            <div>
-                ${content}
-            </div>
-        </div>
-    </body>
-</html>
-`;
+        </body>
+    </html>
+    `;
+    
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
