4b761030c1bb8497a0b1d24e8cef3baee51aefe5
Ravi Vaniya <ravivaniya4911@gmail.com>
2017-08-23 16:01:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,10 +109,10 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
-//SELECT * FROM article where title ='';DELETE WHERE a= 'asdgf'
+//SELECT * FROM article where title ='\';DELETE WHERE a=\'asdgf'
 app.get('/articles/:articleName', function (req, res) {
    // var articleName = req.params.articleName;
-    pool.query("SELECT * FROM article where title  = '"+ req.params.articleName + "'",function(err,result){
+    pool.query("SELECT * FROM article where title  = $1",[req.params.articleName],function(err,result){
         if(err){
             res.status(500).send(err,toString());
         } else {
