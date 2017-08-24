857ebae54f1c8b0bbafafad7d8528e8d0b9e9891
Ravi Vaniya <ravivaniya4911@gmail.com>
2017-08-24 11:48:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-Parser');
+
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+
 var config = {
     user:'ravivaniya4911',
     database:'ravivaniya4911',
@@ -67,6 +71,21 @@ function hash(input,salt){
 app.get('/hash/:input', function (req, res) {
   var hashedString = hash(req.params.input,'this-is-random-string');
   res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+    
+    var username = req.body.username;
+    var password = req.body.password;
+  var salt = crypto.randomBytes(128).toString('hex');
+  var dbString = hash(password,salt);
+  pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+      if(err){
+            res.status(500).send(err,toString());
+        } else {
+            res.send('User created successfully'+username);
+        }
+  });
 });
 
 var pool = new Pool(config);
