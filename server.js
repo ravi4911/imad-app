6550b074d2af607bb5513129e9d66a63a06a534c
Ravi Vaniya <ravivaniya4911@gmail.com>
2017-08-24 21:12:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,6 +129,11 @@ app.get('/check-login',function(req,res){
     }
 });
 
+app.get('/logout',function(req,res){
+    delete req.session.auth;
+    res.send('You are Logout');
+});
+
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select request
