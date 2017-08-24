cd978379f07904cfebb56e32b24b3cc4f212147e
Ravi Vaniya <ravivaniya4911@gmail.com>
2017-08-24 17:22:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.post('/login',function(req,res){
             }else{
                 var dbString = result.rows[0].password;
                 var salt = dbString.split('$')[2];
-                var hashedPassword = hash(password.salt);
+                var hashedPassword = hash(password,salt);
                 if(hashedPassword == dbString){
                     res.send('Creditials correct');
                 }else{
